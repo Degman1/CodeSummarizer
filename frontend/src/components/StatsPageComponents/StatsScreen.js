f26e41a47059d79009ba13c5b782a3bab1583c92
harrisonlogan <100504550@alumnos.uc3m.es>
2024-04-30 19:09:50 -0400

    added stats screen elements

--- frontend/src/components/StatsPageComponents/StatsScreen.js
+++ frontend/src/components/StatsPageComponents/StatsScreen.js
@@ -1,7 +1,47 @@
+import st from './StatsScreen.module.css'
+
 function StatScreen() {
+    function BuildLeftColumn() {
+        return (<div className={st.leftColumn}>{BuildDataBlockLarge('Coding Languages')}{BuildDataBlockLarge('Style Preferences')}</div>)
+    }
+
+    function BuildRightColumn() {
+        return (<div className={st.rightColumn}>
+                {BuildDataBlockSmall('User Info')}
+                {BuildDataBlockSmall('User Data')}
+            </div>)
+    }
+    function BuildDataBlockSmall(header) {
+        return (<div className={st.smallDataBlock}>
+                <h1 className={st.header}>{header}</h1>
+            </div>)
+    }
+    function BuildDataBlockLarge(header, graph_title) {
+        return (<div className={st.largeDataBlock}>
+                <h1 className={st.header}>{header}</h1>
+                {BuildChartRankContainer(graph_title)}
+            </div>)
+    }
+    function BuildChartRankContainer(graph_title) {
+        return (<div className={st.chartRankContainer}>
+            {BuildRankings()}
+            {BuildGraph(0, 0, graph_title)}
+        </div>)
+    }
+    function BuildRankings() {
+        return (<div>
+            <h3>Rank</h3>
+        </div>)
+    }
+    function BuildGraph(data, graph_type, graph_title) {
+        return (<div>
+
+        </div>)
+    }
     return (
-        <div>
-            <h1>Stat sub Screen</h1>
+        <div className={st.container}>
+            {BuildLeftColumn()}
+            {BuildRightColumn()}
         </div>
     );
 }
---
+++ frontend/src/components/StatsPageComponents/UserScreen.js
@@ -0,0 +1,14 @@
+import st from './ProjectList.module.css'
+import react, { useState } from 'react'
+import NewProjectScreen from './NewProjectScreen'
+import SummariesScreen from './SummariesScreen'
+
+function StatScreen() {
+    return (
+        <div>
+            <h1>Stat sub Screen</h1>
+        </div>
+    );
+}
+
+export default StatScreen
