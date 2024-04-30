784f2925ae2423d76c9d439c70c6c092d0a5d620
Misha Lvovsky <m.lvovsky66@gmail.com>
2024-04-30 19:41:24 -0400

    Fixed async stuff

--- backend/summarizer.js
+++ backend/summarizer.js
@@ -42,18 +42,17 @@ class Haiku extends LLM {
   async prompt(text) {
     // Use Haiku to summarize the text
     try {
-      var msg = await anthropic.messages.create({
+      var msg = (await anthropic.messages.create({
         model: "claude-3-haiku-20240307",
         max_tokens: 1024,
-        messages: [{ role: "user", content: task + "\n" + text }],
-      });
-      return msg.data.messages[0].content;
+        messages: [{ role: "user", content: text }],
+      })).content;
+      return msg[0].text;
     } catch (err) {
       console.error(err);
       return "Error: Could not reach Haiku";
     }
   }
-
 }
 
 // instantiate Haiku model
@@ -65,19 +64,19 @@ const haiku = new Haiku();
  * @param programming_language String The name of the programming language for the code snippet
  * @return JSON [{ text: String, catagory: String }]
  */
-export function getSummaries(code_snippet, programming_language) {
+export async function getSummaries(code_snippet, programming_language) {
 
   return [
     {
-      text: haiku.prompt("Summarize the following code snippet\n" + code_snippet),
+      text: await haiku.prompt("Summarize the following " + programming_language + " code snippet\n" + code_snippet),
       category: "Brief Summary",
     },
     {
-      text: haiku.prompt("Rate the following code snippet on a scale of 1 to 10\n" + code_snippet),
+      text: await haiku.prompt("Rate the following " + programming_language + " code snippet on a scale of 1 to 10\n" + code_snippet),
       category: "Rating",
     },
     {
-      text: haiku.prompt("What are the main features of the following code snippet?\n" + code_snippet),
+      text: await haiku.prompt("What are the main features of the following " + programming_language + " code snippet?\n" + code_snippet),
       category: "Features",
     }
   ];
