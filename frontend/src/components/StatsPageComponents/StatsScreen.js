a9e4ae3beb1d338a0495cba77bce3eeca8c21e54
harrisonlogan <100504550@alumnos.uc3m.es>
2024-05-01 17:02:45 -0400

    updated stats page

--- frontend/src/components/StatsPageComponents/StatsScreen.js
+++ frontend/src/components/StatsPageComponents/StatsScreen.js
@@ -17,6 +17,16 @@ function StatScreen() {
     Javascript: 3,
   });
 
+  const [userInfo, setUserInfo] = useState({
+    Name: 'Jacob Sweet',
+    Email: 'jacobsweet@umass.edu',
+  });
+
+  const [userData, setUserData] = useState({
+    'Account Created': '5/1/2024',
+    'Summary Count' : 10
+  });
+
   function BuildDataBlock(header, graph = null) {
     return (
       <div className={st.dataBlock}>
@@ -34,20 +44,45 @@ function StatScreen() {
       </div>
     );
   }
-  function BuildRankings() {
+  function BuildRankings(data) {
+    const sortedArray = Object.entries(data).sort((a, b) => a[1] - b[1]);
+    const sortedDictionary = Object.fromEntries(sortedArray);
     return (
       <div>
-        {
-        Object.keys(languageData).map((key) => {
-          return <li>{key}: {languageData[key]}</li>
-        })
-        }
+        <h3>Ranks</h3>
+        <ol className={st.outerList}>
+          {
+          Object.keys(sortedDictionary).map((key) => {
+            return <li className={st.listItem}>{key}: {sortedDictionary[key]}</li>
+          })
+          }
+        </ol>
       </div>
     );
   }
+  function BuildGraph(data, graph_type) {
+    if (graph_type == 'pie') {
 
-  function BuildGraph(data, graph_type, graph_title) {
-    return <div></div>;
+    }
+    else if (graph_type == 'bar') {
+      
+    }
+    return (<div>
+      
+    </div>);
+  }
+  function BuildUser(data) {
+    return (
+    <div>
+      <ul className={st.userData}>
+          {
+          Object.keys(data).map((key) => {
+            return <li className={st.listItem}>{key}: {data[key]}</li>
+          })
+          }
+        </ul>
+    </div>
+    )
   }
 
   return (
@@ -55,24 +90,32 @@ function StatScreen() {
       <div className={st.leftColumn}>
         <h1 className={st.header}>Coding Languages</h1>
         <div className={st.dataBlock}>
+          <div className={st.detailsArea}>
+            {BuildRankings(languageData)}
+          </div>
           <div className={st.graphArea}>GRAPH AREA</div>
-          <div className={st.detailsArea}>DETAILS AREA</div>
         </div>
         <h1 className={st.header}>Style Preferences</h1>
         <div className={st.dataBlock}>
+          <div className={st.detailsArea}>
+            {BuildRankings(styleData)}
+          </div>
           <div className={st.graphArea}>GRAPH AREA</div>
-          <div className={st.detailsArea}>DETAILS AREA</div>
         </div>
       </div>
 
       <div className={st.rightColumn}>
         <h1 className={st.header}>User Info</h1>
         <div className={st.dataBlock}>
-          <div className={st.userDetailsArea}>DETAILS AREA</div>
+          <div className={st.userDetailsArea}>
+            {BuildUser(userInfo)}
+          </div>
         </div>
-        <h1 className={st.header}>Style Preferences</h1>
+        <h1 className={st.header}>User Data</h1>
         <div className={st.dataBlock}>
-          <div className={st.userDetailsArea}>User Data</div>
+          <div className={st.userDetailsArea}>
+            {BuildUser(userData)}
+          </div>
         </div>
       </div>
     </div>
